ebe480a9e594ad18a8895093a4305357b4e08ec2
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-08 20:18:40 -0400

    Create exports

---
+++ src/components/index.jsx
@@ -0,0 +1,9 @@
+import AppContainer from './AppContainer/AppContainer';
+import Navbar from './Navbar/Navbar';
+import InfoSection from './InfoSection/InfoSection';
+
+export {
+     AppContainer,
+     Navbar,
+     InfoSection,
+};
