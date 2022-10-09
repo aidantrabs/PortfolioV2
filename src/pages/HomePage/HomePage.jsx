56cf4ed35eda04c61a3a71f63cc103052c2bbf87
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-08 20:19:05 -0400

    Create HomePage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './HomePage.css';
+
+import { Navbar, InfoSection } from "../../components";
+
+const HomePage = () => {
+     return (
+          <>
+               <Navbar />
+               <InfoSection />
+          </>
+     );
+};
+
+export default HomePage;
