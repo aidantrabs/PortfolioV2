86552fa7cd385d1a08c7eceedb80841a523de641
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-08 20:18:17 -0400

    Create InfoSection component

---
+++ src/components/InfoSection/InfoSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './InfoSection.css';
+
+const InfoSection = () => {
+     return (
+          <>
+               <div className="info-container">
+                    <div className="info-wrapper">
+                         <h1>Hi, I'm Aidan Traboulay</h1>
+                    </div>
+               </div>
+          </>
+     );
+};
+
+export default InfoSection;
