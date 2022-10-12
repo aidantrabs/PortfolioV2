82b7af11985035c986a2fef48ed56eb275c9a736
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-12 06:43:36 -0400

    Update homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import './HomePage.css';
-
 import { Navbar, HeroSection, AboutSection, ExperienceSection, ProjectsSection, ContactSection } from "../../components";
 
 const HomePage = () => {
