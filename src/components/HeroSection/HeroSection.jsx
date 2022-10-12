4c1fcdb031abb84dc0d77d81181707bdc16b75a1
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-12 00:31:25 -0400

    Create hero section component and add styling

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './HeroSection.scss';
+import { Button, Container, MediaQuery, Text } from '@mantine/core';
+import { Link } from 'react-scroll';
+import { AiOutlineArrowDown } from 'react-icons/ai';
+
+const HeroSection = () => {
+     return (
+          <section id="hero">
+               <Container fluid>
+                    <div className="hero__container">
+                         <div style={{ marginBottom: 30 }}>
+                              <Text>
+                                   <MediaQuery query="(max-width: 768px)" styles={{ fontSize: '2.8rem !important' }}>
+                                        <h1 className="title">Aidan Traboulay</h1>
+                                   </MediaQuery>                              
+                              </Text>
+                         </div>
+
+                         <div style={{ marginBottom: 25 }}>
+                              <Text size="xl" color="black">
+                                   Hi there, I'm a third year Computer Science Student @ wlu
+                              </Text>
+                         </div>
+
+                         <div className="button__container">
+                              <Link to="about" smooth duration={400}>
+                                   <Button color="blue" size="md" rightIcon={<AiOutlineArrowDown/>}>Learn More</Button>
+                              </Link>
+                         </div>
+                    </div>
+               </Container>
+          </section>
+     );
+};
+
+export default HeroSection;
