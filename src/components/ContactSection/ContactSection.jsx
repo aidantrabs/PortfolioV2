a11b3884bb09161f221bb84424400d2a64469952
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-12 06:44:43 -0400

    Update contact section

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import './ContactSection.scss';
-import { Title, Divider, Text } from '@mantine/core';
+import { Container, Title, Divider, Text } from '@mantine/core';
 import { SocialBar } from '../index';
 
 const ContactSection = () => {
      return (
           <section id="contact">
-               <Title order={1} align="left">Contact</Title>
-               <Divider />
-               <Text size="lg" color="dimmed">
-                    If you have any questions or would like to get in touch, please feel free to contact me.
-               </Text>
-               <SocialBar />
+               <Container>
+                    <Title order={1} align="left">Contact</Title>
+                    <div className="spacer" style={{ width: "100%", height: "1.5em" }} />
+                    <Divider />
+                    <div className="spacer" style={{ width: "100%", height: "2em" }} />
+                    
+                    <Text size="lg" color="dimmed">
+                         If you have any questions or would like to get in touch, please feel free to contact me.
+                    </Text>
+
+                    <div className="spacer" style={{ width: "100%", height: "2em" }} />
+
+                    <SocialBar />
+               </Container>
           </section>
      );
 };
