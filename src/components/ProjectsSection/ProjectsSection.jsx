912801dfa67c0ebc6a5450642ae9e617996a98ec
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-12 06:45:36 -0400

    Update projects section

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,92 +1,96 @@
 import React from 'react';
 import './ProjectsSection.scss';
-import { Button, Badge, Card, Group, Grid, Image, Text, Title, Divider } from '@mantine/core';
+import { Button, Badge, Container, Card, Group, Grid, Image, Text, Title, Divider } from '@mantine/core';
 
 const ProjectsSection = () => {
      return (
           <section id="projects">
                <div className="projects__container">
-                    <Title order={1} align="center">Projects</Title>
-                    <Divider />
-                    <Grid grow>
-                         <Grid.Col md={6} lg={3}>
-                              {/* Project 1 */}
-                              <Card shadow="sm" p="lg" radius="lg" withBorder>
-                                   <Card.Section>
-                                        <Image 
+                    <Container>
+                         <Title order={1} align="center">Projects</Title>
+                         <div className="spacer" style={{ width: "100%", height: "1.5em" }} />
+                         <Divider />
+                         <div className="spacer" style={{ width: "100%", height: "2em" }} />
+                         <Grid grow>
+                              <Grid.Col md={6} lg={3}>
+                                   {/* Project 1 */}
+                                   <Card shadow="sm" p="lg" radius="lg" withBorder>
+                                        <Card.Section>
+                                             <Image 
+                                                  src=""
+                                                  height={150}
+                                                  alt="Project 1"
+                                             />
+                                        </Card.Section>
+
+                                        <Group positon="apart" mt="md" mb="xs">
+                                             <Text weight={500}>This my 1st project card</Text>
+                                             <Badge color="blue" variant="light"> Cybersecurity </Badge>
+                                        </Group>
+
+                                        <Text size="sm" color="dimmed">
+                                             This is a description of my 1st project card
+                                        </Text>
+
+                                        <Button variant="light" color="blue" mt="md" radius="md" fullWidth>
+                                             View Project Code
+                                        </Button>
+                                   </Card>
+                              </Grid.Col>
+
+                              <Grid.Col md={6} lg={3}>
+                                   {/* Project 2 */}
+                                   <Card shadow="sm" p="lg" radius="lg" withBorder>
+                                        <Card.Section>
+                                             <Image 
                                              src=""
                                              height={150}
-                                             alt="Project 1"
-                                        />
-                                   </Card.Section>
+                                             alt="Project 2"
+                                             />
+                                        </Card.Section>
 
-                                   <Group positon="apart" mt="md" mb="xs">
-                                        <Text weight={500}>This my 1st project card</Text>
-                                        <Badge color="blue" variant="light"> Cybersecurity </Badge>
-                                   </Group>
+                                        <Group positon="apart" mt="md" mb="xs">
+                                             <Text weight={500}>This my 2nd project card</Text>
+                                             <Badge color="blue" variant="light"> Cybersecurity </Badge>
+                                        </Group>
 
-                                   <Text size="sm" color="dimmed">
-                                        This is a description of my 1st project card
-                                   </Text>
+                                        <Text size="sm" color="dimmed">
+                                             This is a description of my 2nd project card
+                                        </Text>
 
-                                   <Button variant="light" color="blue" mt="md" radius="md" fullWidth>
-                                        View Project Code
-                                   </Button>
-                              </Card>
-                         </Grid.Col>
+                                        <Button variant="light" color="blue" mt="md" radius="md" fullWidth>
+                                             View Project Code
+                                        </Button>
+                                   </Card>
+                              </Grid.Col>
 
-                         <Grid.Col md={6} lg={3}>
-                              {/* Project 2 */}
-                              <Card shadow="sm" p="lg" radius="lg" withBorder>
-                                   <Card.Section>
-                                        <Image 
-                                        src=""
-                                        height={150}
-                                        alt="Project 2"
-                                        />
-                                   </Card.Section>
+                              <Grid.Col md={6} lg={3}>
+                                   {/* Project 3 */}
+                                   <Card shadow="sm" p="lg" radius="lg" withBorder>
+                                        <Card.Section>
+                                             <Image 
+                                             src=""
+                                             height={150}
+                                             alt="Project 3"
+                                             />
+                                        </Card.Section>
 
-                                   <Group positon="apart" mt="md" mb="xs">
-                                        <Text weight={500}>This my 2nd project card</Text>
-                                        <Badge color="blue" variant="light"> Cybersecurity </Badge>
-                                   </Group>
+                                        <Group positon="apart" mt="md" mb="xs">
+                                             <Text weight={500}>This my 3rd project card</Text>
+                                             <Badge color="blue" variant="light"> Cybersecurity </Badge>
+                                        </Group>
 
-                                   <Text size="sm" color="dimmed">
-                                        This is a description of my 2nd project card
-                                   </Text>
+                                        <Text size="sm" color="dimmed">
+                                             This is a description of my 3rd project card
+                                        </Text>
 
-                                   <Button variant="light" color="blue" mt="md" radius="md" fullWidth>
-                                        View Project Code
-                                   </Button>
-                              </Card>
-                         </Grid.Col>
-
-                         <Grid.Col md={6} lg={3}>
-                              {/* Project 3 */}
-                              <Card shadow="sm" p="lg" radius="lg" withBorder>
-                                   <Card.Section>
-                                        <Image 
-                                        src=""
-                                        height={150}
-                                        alt="Project 3"
-                                        />
-                                   </Card.Section>
-
-                                   <Group positon="apart" mt="md" mb="xs">
-                                        <Text weight={500}>This my 3rd project card</Text>
-                                        <Badge color="blue" variant="light"> Cybersecurity </Badge>
-                                   </Group>
-
-                                   <Text size="sm" color="dimmed">
-                                        This is a description of my 3rd project card
-                                   </Text>
-
-                                   <Button variant="light" color="blue" mt="md" radius="md" fullWidth>
-                                        View Project Code
-                                   </Button>
-                              </Card>
-                         </Grid.Col>
-                    </Grid>
+                                        <Button variant="light" color="blue" mt="md" radius="md" fullWidth>
+                                             View Project Code
+                                        </Button>
+                                   </Card>
+                              </Grid.Col>
+                         </Grid>
+                    </Container>
                </div>
           </section>
      );
