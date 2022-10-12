a53ff7aaa859f59750ea817bc637efe37960ed97
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-12 00:35:16 -0400

    Create social bar component and update styling

---
+++ src/components/SocialBar/SocialBar.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Center, Grid, ActionIcon } from "@mantine/core";
+import { AiFillInstagram, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { MdOutlineContactPage } from "react-icons/md";
+
+const SocialBar = () => {
+    return (
+        <Center>
+            <Grid align="left" grow>
+                <Grid.Col span={3}>
+                    <a href="https://instagram.com/aidantraboulayy" target="_blank" rel="external noreferrer">
+                        <ActionIcon size="xl">
+                            <AiFillInstagram size={100} color="#02010A"/>  
+                        </ActionIcon>
+                    </a>
+                </Grid.Col>
+
+                <Grid.Col span={3}>
+                    <a href="https://linkedin.com/in/aidan-traboulay" target="_blank" rel="external noreferrer">
+                        <ActionIcon size="xl">
+                            <AiFillLinkedin size={100} color="#02010A"/>
+                        </ActionIcon>
+                    </a>
+                </Grid.Col>
+
+                <Grid.Col span={3}>
+                    <a href="https://github.com/aidantrabs" target="_blank" rel="external noreferrer">
+                        <ActionIcon size="xl">
+                            <AiFillGithub size={100} color="#02010A"/>
+                        </ActionIcon>
+                    </a>
+                </Grid.Col>
+
+                <Grid.Col span={3}>
+                    <a href="#" target="_blank" rel="next noreferrer">
+                        <ActionIcon size="xl">
+                            <MdOutlineContactPage size={100} color="#02010A"/>
+                        </ActionIcon>
+                    </a>
+                </Grid.Col>
+            </Grid>
+        </Center>
+    );
+};
+
+export default SocialBar;
+
+// 
