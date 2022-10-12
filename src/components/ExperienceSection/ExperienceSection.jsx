3bfcf972750769dd2182107e4065738bee4bacc8
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-12 06:45:01 -0400

    Update experience section

--- src/components/ExperienceSection/ExperienceSection.jsx
+++ src/components/ExperienceSection/ExperienceSection.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import './ExperienceSection.scss';
 
-import { Divider, Timeline, Text, Title } from '@mantine/core';
+import { Container, Divider, Grid, Timeline, Text, Title } from '@mantine/core';
 
 const ExperienceSection = () => {
      return (
           <section id="experience">
-               <Title order={1} align="right">Experience</Title>
-               <Divider />
-               <Timeline active={1} color="dark" lineWidth={3} bulletSize={24} align="right">
-                    <Timeline.Item title="Vice President of Development & Technology" >
-                         <Text>  </Text>
-                         <Text size="xs"> 2022 - 2023512 </Text>
-                    </Timeline.Item>
+               <Container>
+                    <Title order={1} align="left">Experience</Title>
+                    <div className="spacer" style={{ width: "100%", height: "1.5em" }} />
+                    <Divider />
+                    <div className="spacer" style={{ width: "100%", height: "2em" }} />
+                    <Grid justify="space-around"> 
+                         <Grid.Col>    
+                              <Timeline active={1} color="dark" lineWidth={3} bulletSize={24} align="left">
+                                   <Timeline.Item title="Vice President of Development & Technology" >
+                                        <Text>  </Text>
+                                        <Text size="xs"> 2022 - 2023512 </Text>
+                                   </Timeline.Item>
 
-                    <Timeline.Item title="Vice President of Development & Technology">
-                         <Text> i did xyz </Text>
-                         <Text> 2022 - 2023512 </Text>
-                    </Timeline.Item>
+                                   <Timeline.Item title="Vice President of Development & Technology">
+                                        <Text> i did xyz </Text>
+                                        <Text> 2022 - 2023512 </Text>
+                                   </Timeline.Item>
 
-                    <Timeline.Item title="Vice President of Development & Technology">
-                         <Text> i did xyz </Text>
-                         <Text> 2022 - 2023512 </Text>
-                    </Timeline.Item>
-               </Timeline>
+                                   <Timeline.Item title="Vice President of Development & Technology">
+                                        <Text> i did xyz </Text>
+                                        <Text> 2022 - 2023512 </Text>
+                                   </Timeline.Item>
+                              </Timeline>
+                         </Grid.Col>
+                    </Grid>
+               </Container>
           </section>
      );
 };
