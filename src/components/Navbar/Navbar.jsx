21746612bcec8cc9cda5e7888d1128e068e61c60
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-12 00:37:56 -0400

    Update navbar and update styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,61 @@
 import React from 'react';
-import './Navbar.css';
+import './Navbar.scss';
+import { Anchor, Button, Burger, Drawer, Text, Title } from '@mantine/core';
+import { Link } from 'react-scroll';
+import { SocialBar } from '../../components';
 
 const Navbar = () => {
+
+     const [opened, setOpened] = React.useState(false);
+
      return (
-          <>
-               <nav className="navbar">
-                    <div className="navbar__logo">
-                         <h1>Aidan Traboulay</h1>
+          <header>
+               <div className="navbar__desktop">
+                    <div>
+
                     </div>
-                    <div className="navbar__links">
-                         <ul>
-                              <li><a href="#">Home</a></li>
-                              <li><a href="#">Projects</a></li>
-                              <li><a href="#">Resume</a></li>
-                              <li><a href="#">Contact</a></li>
-                         </ul>
+                    <div className="navbar">
+                         <div className="navbar__item"><Link to="about" smooth duration={400}>About</Link></div>
+                         <div className="navbar__item"><Link to="experience" smooth duration={400}>Experience</Link></div>
+                         <div className="navbar__item"><Link to="projects" smooth duration={400}>Projects</Link></div>
+
+                         <Button color="" size="sm"> Contact </Button>
                     </div>
-               </nav>
-          </>
+               </div>
+
+               <div className="navbar__mobile">
+                    <div className="burger__button">
+                         <Burger
+                              opened={opened}
+                              onClick={() => setOpened((open) => !open)}
+                              size="sm"
+                         />
+                    </div>
+
+                    <Drawer 
+                         transition="rotate-right"
+                         transitionDuration={300}
+                         transitionTimingFunction="ease"
+                         opened={opened}
+                         onClose={() => setOpened(false)}
+                         position="right"
+                         overlayOpacity={0.5}
+                         padding="xl"
+                    >
+                         <div className="drawer__content">
+                              <div className="drawer__items">
+                                   <div className="drawer__item"><Link to="#" smooth duration={400} onClick={() => setOpened(false)}><Title order={2}>About</Title></Link></div>
+                                   <div className="drawer__item"><Link to="#" smooth duration={400} onClick={() => setOpened(false)}><Title order={2}>Experience</Title></Link></div>
+                                   <div className="drawer__item"><Link to="#" smooth duration={400} onClick={() => setOpened(false)}><Title order={2}>Projects</Title></Link></div>
+                              </div>
+
+                              <div className="drawer__items">
+                                   <SocialBar/>
+                              </div>
+                         </div>
+                    </Drawer>
+               </div>
+          </header>
      );
 };
 
