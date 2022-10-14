eb367f18e18a835d44de24ce5976b44ec04a44e2
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-14 05:01:01 -0400

    Update asset exports

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,9 +1,11 @@
 import Headshot from './professional_headshot.jpg';
 import Brand from './brand.png';
 import Wave from './wave.svg';
+import Gradient from './gradiant-placeholder.jpg';
 
 export {
      Headshot,
      Brand,
      Wave,
+     Gradient,
 }
