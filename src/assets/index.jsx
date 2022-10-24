cf61f1d7380278071f5f2414e9b2a0a755f365c5
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-24 04:15:52 -0400

    Update assets and exports

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,10 +2,14 @@ import Headshot from './professional_headshot.jpg';
 import Brand from './brand.png';
 import Wave from './wave.svg';
 import Gradient from './gradiant-placeholder.jpg';
+import Moon from './moon.svg';
+import Programmer from './programmer.svg';
 
 export {
      Headshot,
      Brand,
      Wave,
      Gradient,
+     Moon,
+     Programmer
 }
