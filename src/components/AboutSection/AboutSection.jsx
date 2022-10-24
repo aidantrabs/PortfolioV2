2e41853f4ff9591f3217cbd3cb98261d78037882
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-10-24 04:16:34 -0400

    Update about section component

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -15,7 +15,7 @@ const AboutSection = () => {
                                         <div className="spacer" style={{ width: "100%", height: "1.5em" }} />                                       
                                         <Divider />
                                         <div className="spacer" style={{ width: "100%", height: "2em" }} />
-                                        <Blockquote color="blue">
+                                        <Blockquote color="btn-color.3">
                                              <Text color="white">
                                                   I'm a third year Computer Science student at Wilfrid Laurier University. I'm passionate about building software that is both functional and beautiful. I'm currently looking for a co-op position for the summer of 2022.
                                              </Text>
