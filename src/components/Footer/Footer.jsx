cbfa38ab0d783f1365b44985b002ddce1689b53f
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-05 07:18:58 -0400

    Create footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.scss";
+import { SocialBar } from "../../components";
+
+const Footer = () => {
+     return (
+          <footer>
+               <div className="footer-container">
+                    <div className="footer__info">
+                         <p>© 2022 Aidan Traboulay. All rights reserved.</p>
+                         <SocialBar />
+                    </div>
+               </div>
+          </footer>
+     );
+};
+
+export default Footer;
