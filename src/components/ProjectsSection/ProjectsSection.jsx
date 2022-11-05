eed2edf299b3b31b4bc6dc80c59a0626b1a08db8
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-05 07:18:03 -0400

    Update projects section and styling

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ProjectsSection.scss';
-import { Button, Badge, Container, Card, Group, Grid, Image, Text, Title, Divider } from '@mantine/core';
+import { Button, Badge, Container, Card, Group, Image, Text, Title, Divider } from '@mantine/core';
+import { Carousel } from '@mantine/carousel';
 import { Gradient } from '../../assets';
 
 const ProjectsSection = () => {
@@ -8,89 +9,94 @@ const ProjectsSection = () => {
           <section id="projects">
                <div className="projects__container">
                     <Container>
-                         <Title order={1} align="left" color={"white"}>Projects</Title>
+                         <Title order={1} align="left" color={"white"}> Projects💻 </Title>
                          <div className="spacer" style={{ width: "100%", height: "1.5em" }} />
                          <Divider />
                          <div className="spacer" style={{ width: "100%", height: "2em" }} />
-                         <Grid grow>
-                              <Grid.Col md={6} lg={3}>
+                         <Carousel slideSize="50%" height={500} slideGap="lg" loop dragFree slidesToScroll={1} 
+                              breakpoints={[
+                                   { maxWidth: 'md', slideSize: '50%' },
+                                   { maxWidth: 'sm', slideSize: '100%', slideGap: -10 },
+                              ]}
+                         >
                                    {/* Project 1 */}
-                                   <Card shadow="sm" p="lg" radius="lg" withBorder>
-                                        <Card.Section>
-                                             <Image 
+                                   <Carousel.Slide>
+                                        <Card shadow="sm" p="lg" radius="lg" withBorder>
+                                             <Card.Section>
+                                                  <Image 
+                                                       src={Gradient}
+                                                       height={150}
+                                                       alt="Project 1"
+                                                  />
+                                             </Card.Section>
+
+                                             <Group positon="apart" mt="md" mb="xs">
+                                                  <Text weight={500}> Rendezvous </Text>
+                                                  <Badge color="btn-color.6" variant="dark"> Cybersecurity </Badge>
+                                             </Group>
+
+                                             <Text size="sm" color="dimmed">
+                                                  A Python Discord bot which uses the Pycord library and the Ticketmaster API, hosted on Google Cloud's Compute Engine which allows users to filter events worldwide.
+                                             </Text>
+
+                                             <Button variant="dark" color="btn-color.6" mt="md" radius="md" fullWidth>
+                                                  View Project Code
+                                             </Button>
+                                        </Card>
+                                   </Carousel.Slide>
+
+                                   {/* Project 2 */}
+                                   <Carousel.Slide>
+                                        <Card shadow="sm" p="lg" radius="lg" withBorder>
+                                             <Card.Section>
+                                                  <Image 
                                                   src={Gradient}
                                                   height={150}
-                                                  alt="Project 1"
-                                             />
-                                        </Card.Section>
+                                                  alt="Project 2"
+                                                  />
+                                             </Card.Section>
 
-                                        <Group positon="apart" mt="md" mb="xs">
-                                             <Text weight={500}>This my 1st project card</Text>
-                                             <Badge color="blue" variant="light"> Cybersecurity </Badge>
-                                        </Group>
+                                             <Group positon="apart" mt="md" mb="xs">
+                                                  <Text weight={500}> TechHunt </Text>
+                                                  <Badge color="btn-color.6" variant="dark"> Cybersecurity </Badge>
+                                             </Group>
 
-                                        <Text size="sm" color="dimmed">
-                                             This is a description of my 1st project card
-                                        </Text>
+                                             <Text size="sm" color="dimmed">
+                                             Open source full stack application that allows software developers to search for, upload, favorite, comment and request to join other open source projects. 
+                                             </Text>
 
-                                        <Button variant="light" color="blue" mt="md" radius="md" fullWidth>
-                                             View Project Code
-                                        </Button>
-                                   </Card>
-                              </Grid.Col>
+                                             <Button variant="dark" color="btn-color.6" mt="md" radius="md" fullWidth>
+                                                  View Project Code
+                                             </Button>
+                                        </Card>
+                                   </Carousel.Slide>
 
-                              <Grid.Col md={6} lg={3}>
-                                   {/* Project 2 */}
-                                   <Card shadow="sm" p="lg" radius="lg" withBorder>
-                                        <Card.Section>
-                                             <Image 
-                                             src={Gradient}
-                                             height={150}
-                                             alt="Project 2"
-                                             />
-                                        </Card.Section>
-
-                                        <Group positon="apart" mt="md" mb="xs">
-                                             <Text weight={500}>This my 2nd project card</Text>
-                                             <Badge color="blue" variant="light"> Cybersecurity </Badge>
-                                        </Group>
-
-                                        <Text size="sm" color="dimmed">
-                                             This is a description of my 2nd project card
-                                        </Text>
-
-                                        <Button variant="light" color="blue" mt="md" radius="md" fullWidth>
-                                             View Project Code
-                                        </Button>
-                                   </Card>
-                              </Grid.Col>
-
-                              <Grid.Col md={6} lg={3}>
                                    {/* Project 3 */}
-                                   <Card shadow="sm" p="lg" radius="lg" withBorder>
-                                        <Card.Section>
-                                             <Image 
-                                             src={Gradient}
-                                             height={150}
-                                             alt="Project 3"
-                                             />
-                                        </Card.Section>
+                                   <Carousel.Slide>
+                                        <Card shadow="sm" p="lg" radius="lg" withBorder>
+                                             <Card.Section>
+                                                  <Image 
+                                                  src={Gradient}
+                                                  height={150}
+                                                  alt="Project 3"
+                                                  />
+                                             </Card.Section>
 
-                                        <Group positon="apart" mt="md" mb="xs">
-                                             <Text weight={500}>This my 3rd project card</Text>
-                                             <Badge color="blue" variant="light"> Cybersecurity </Badge>
-                                        </Group>
+                                             <Group positon="apart" mt="md" mb="xs">
+                                                  <Text weight={500}> TabThat </Text>
+                                                  <Badge color="btn-color.6" variant="dark"> Cybersecurity </Badge>
+                                             </Group>
 
-                                        <Text size="sm" color="dimmed">
-                                             This is a description of my 3rd project card
-                                        </Text>
+                                             <Text size="sm" color="dimmed">
+                                             REST API to create, read, update and delete bookmarks, which sends and recieves data via a database. Built using Go, Gin Web Framework and MongoDB. 
+                                             </Text>
 
-                                        <Button variant="light" color="blue" mt="md" radius="md" fullWidth>
-                                             View Project Code
-                                        </Button>
-                                   </Card>
-                              </Grid.Col>
-                         </Grid>
+                                             <Button variant="dark" color="btn-color.6" mt="md" radius="md" fullWidth>
+                                                  View Project Code
+                                             </Button>
+                                        </Card>
+                                   </Carousel.Slide>
+                              </Carousel>
                     </Container>
                </div>
           </section>
