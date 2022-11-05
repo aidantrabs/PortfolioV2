58d7b5d9eb35dd7fbaac989211f4667814c02069
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-05 07:17:07 -0400

    Update hero section and styling

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,41 +1,21 @@
 import React from 'react';
 import './HeroSection.scss';
-import { Button, Container, MediaQuery, Text, Image } from '@mantine/core';
+import ParticlesBg from "particles-bg";
+import { Button, Container, MediaQuery, Text, Image, Grid, Title, Divider, Blockquote } from '@mantine/core';
 import { Link } from 'react-scroll';
 import { AiOutlineArrowDown } from 'react-icons/ai';
-import { Programmer } from '../../assets';
+import { Hero } from '../../assets';
+
 
 const HeroSection = () => {
      return (
           <>
-          <section id="hero">
-               <Container fluid>
-                    <div className="hero__container">
-                         <div style={{ marginBottom: 30 }}>
-                              <Text>
-                                   <MediaQuery query="(max-width: 768px)" styles={{ fontSize: '2.8rem !important' }}>
-                                        <h1 className="title">Aidan Traboulay</h1>
-                                   </MediaQuery>     
-
-                                   <Image src={Programmer} alt="programmer" width="80%"/>
-
-                              </Text>
-                         </div>
-
-                         <div style={{ marginBottom: 25 }}>
-                              <Text size="xl" color="white">
-                                   Hi there, I'm a third year Computer Science Student @ wlu
-                              </Text>
-                         </div>
-
-                         <div className="button__container">
-                              <Link to="about" smooth duration={400}>
-                                   <Button color="btn-color.3" size="md" rightIcon={<AiOutlineArrowDown/>}>Learn More</Button>
-                              </Link>
-                         </div>
-                    </div>
-               </Container>
-          </section>
+               <section id="hero">
+                    <Container fluid>
+                         <ParticlesBg color="#ffffff" num={120} type="cobweb" bg={true} />
+                         <p>Test</p>
+                    </Container>
+               </section>
           </>
      );
 };
