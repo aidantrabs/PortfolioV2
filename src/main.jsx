ac8890cdd1693a1489bc6f016a83ae93bc37ca99
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-05 07:14:44 -0400

    Update main

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         colors: {
           'primary-purple': ["#E8E5F3","#CDC4E8","#B2A2E4","#977BE7","#7950F2","#6B45DD","#603EC7","#5C42A9","#57448F","#52447A","#4C4269"],
           'secondary-purple': ["#FDFAFB","#F3D6E0","#F1AFC6","#F783AC","#E66E99","#D26088","#BF557A","#A94F6F","#904F66","#7C4D5E"],
-          'btn-color': ["#FDFAFB","#F3D6E0","#F1AFC6","#F783AC","#E66E99","#D26088","#BF557A","#A94F6F","#904F66","#7C4D5E"]
+          'btn-color':["#D1B1B9","#CA96A3","#C7798C","#C95975","#D3355D","#D81848","#DA0037","#B1143B","#91203C","#78273B","#652A39","#552B36","#492B32"
+          ]
         }
       }}
     >
